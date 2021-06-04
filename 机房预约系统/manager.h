7092344dc2dc999a7cae61b58a1c5846a8b9d360
Ironman <Ironman@DESKTOP-LUK117I>
2021-06-04 22:45:45 +0800

    添加项目文件。

---
+++ 机房预约系统/manager.h
@@ -0,0 +1,46 @@
+#pragma once
+#include<iostream>
+using namespace std;
+#include"ldentity.h"
+#include<fstream>
+#include<vector>
+#include"student.h"
+#include"teacher.h"
+#include<algorithm>
+#include"computerRoom.h"
+class Manager :public Identity
+{
+public:
+
+	//默认构造
+	Manager();
+
+	//有参构造  管理员姓名，密码
+	Manager(string name, string pwd);
+
+	//选择菜单
+	virtual void operMenu();
+
+	//添加账号  
+	void addPerson();
+
+	//查看账号
+	void showPerson();
+
+	//查看机房信息
+	void showComputer();
+
+	//清空预约记录
+	void cleanFile();
+
+	//初始化
+	void initVector();
+
+	//检测重复
+	bool checkRepeat(int id, int type);
+
+	vector<Student>vStu;
+	vector<Teacher>vTea;
+
+	vector<ComputerRoom> vCom;
+};
